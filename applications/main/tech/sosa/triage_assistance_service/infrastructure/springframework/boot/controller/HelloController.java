239f773e05b26df907994996fc47757c744cf6e2
Miguel González <sosa@uma.es>
2019-05-10 23:33:28 +0200

    End of first iteration.

---
+++ applications/main/tech/sosa/triage_assistance_service/infrastructure/springframework/boot/controller/HelloController.java
@@ -0,0 +1,14 @@
+package tech.sosa.triage_assistance_service.infrastructure.springframework.boot.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
